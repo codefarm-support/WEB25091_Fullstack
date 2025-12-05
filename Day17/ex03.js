d0e6649feda7bd45c0a9262f067e431cc3a4579c
CodeFarm <support@codefarm.edu.vn>
2025-12-05 11:21:21 +0700

    BTTH Day 17

---
+++ Day17/ex03.js
@@ -0,0 +1,9 @@
+/**
+ * * MAP dữ liệu API sang dạng bảng
+ * * VD: Id: 1, name: Viet, Full Address : Hanoi-Cau Giay
+ */
+
+const users = [
+  { id: 1, name: "Viet", address: { city: "Hanoi", district: "Cau Giay" } },
+  { id: 2, name: "Trang", address: { city: "HCM", district: "Q1" } },
+];
