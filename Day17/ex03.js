1d8db7c084f88832d4d9e9291030066e11f159b8
CodeFarm <support@codefarm.edu.vn>
2025-12-05 11:26:05 +0700

    BTTH Day 17

--- Day17/ex03.js
+++ Day17/ex03.js
@@ -7,3 +7,9 @@ const users = [
   { id: 1, name: "Viet", address: { city: "Hanoi", district: "Cau Giay" } },
   { id: 2, name: "Trang", address: { city: "HCM", district: "Q1" } },
 ];
+
+/**
+ * * Chuyển chuỗi thành mảng object có cấu trúc
+ * * VD: str= "viet:8,trang:9.5,minh:7"
+ * * -> [{name: viet, score:8},{name: trang, score: 9.5},{name: minh,score: 7}]
+ */
